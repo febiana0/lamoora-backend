da0c3b86856925e4e428b27964b8bab2bcb00eeb
Febiana Najwah <febiana1512@gmail.com>
2025-05-25 01:28:42 +0700

    rename custom dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace App\Filament\Pages;
-
-use Filament\Pages\Page;
-use App\Filament\Widgets\DashboardStats;
-
 class Dashboard extends Page
 {
     protected static string $view = 'filament.pages.custom_dashboard';
+
+    protected static string $routeName = 'filament.admin.pages.custom_dashboard';
 
     protected function getHeaderWidgets(): array
     {
