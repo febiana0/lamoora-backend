b605253160d310799da458c2d12d099b305f2ee7
Febiana Najwah <febiana1512@gmail.com>
2025-05-25 02:32:18 +0700

    menambah file

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    protected $dontReport = [
+        //
+    ];
+
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthenticationException) {
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Unauthenticated.'], 401);
+            }
+        }
+
+        return parent::render($request, $exception);
+    }
+}
